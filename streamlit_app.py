43f523c7f52027629f3eafdd72ab7122c6c46de4
DavendrenG <88140388+DavendrenG@users.noreply.github.com>
2022-12-09 11:16:49 +0200

    Update streamlit_app.py
    added header and some test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('davendren test')
+streamlit.text('does this work')
