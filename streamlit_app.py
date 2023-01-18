62885ce147fb7afa085ee628045157ea93049571
DavendrenG <88140388+DavendrenG@users.noreply.github.com>
2023-01-18 14:23:37 +0200

    Update streamlit_app.py
    code refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 # importing the python requests library
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # tabulate the pandas response & stored it as a variable - it actually normalized it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
